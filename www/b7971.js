ba29dad7f4e3c7fa28916c9563487b39e6e22c56
Luc Small <luc.small@gmail.com>
2011-08-13 13:47:44 +1000

    Added default patterns, ability to show and remove patterns, and restore default list. I feel a refactoring coming on...

--- www/b7971.js
+++ www/b7971.js
@@ -59,6 +59,8 @@ $(document).ready(function(){
       component.addClass("bit_is_0");          
     }
     
+    load_default_patterns();
+    
     sort_by_segments();
  }
 
@@ -115,6 +117,13 @@ $(document).ready(function(){
     return bits;
  }
  
+  function load_pattern(new_pattern)
+  {
+    $("img.tile").each(function(){    
+      var seg = $(this).data("seg");
+      $(this).data("state", (((new_pattern >> seg) & 0x01) ? 1 : 0));
+    });
+  }
 
 
  function render_state() {
@@ -160,16 +169,18 @@ $(document).ready(function(){
       
       tr = $("<tr/>").appendTo($("#value_table"));
       $("<td width='5%' class='number'/>").appendTo(tr).text(index);
-      $("<td width='10%' class='number'/>").appendTo(tr).text(bits);
-      $("<td width='10%' class='number'/>").appendTo(tr).text(value_as_hex(bits));
-      $("<td width='30%' class='number'/>").appendTo(tr).text(value_as_binary(bits));
-      $("<td width='45%' class='comment'/>").appendTo(tr).text("// " + comments[index]);
+      $("<td width='10%' class='number'/>").appendTo(tr).html("<a href='#' class='action' data-action='show' data-index='" + index + "'>" + bits + "</a>");
+      $("<td width='10%' class='number'/>").appendTo(tr).html("<a href='#' class='action' data-action='show' data-index='" + index + "'>" + value_as_hex(bits) + "</a>");
+      $("<td width='30%' class='number'/>").appendTo(tr).html("<a href='#' class='action' data-action='show' data-index='" + index + "'>" + value_as_binary(bits) + "</a>");
+      $("<td width='40%' class='comment'/>").appendTo(tr).text("// " + comments[index]);
+      $("<td width='5%'/>").appendTo(tr).html("<a href='#' class='action' data-action='remove' data-index='" + index + "'>Remove</a>");
+      
       
       export_code += value_as_hex(bits) + ", // " + index.toString() + " - " + comments[index] + "\n"
     });
     
     
-    $("#value_table_container").animate({ scrollTop: $("#value_table_container").attr("scrollHeight") }, 3000);  
+      
     
     $("#export_text").val("uint16_t pattern[] = {\n" + export_code + "};\n");
   }
@@ -190,7 +201,6 @@ $(document).ready(function(){
   $("img.tile").click(function() {
     toggle_state($(this));
     render_state();
-    
   });
   
   // toggle corresponding segment when component clicked on
@@ -217,6 +227,7 @@ $(document).ready(function(){
     render_list();
     // clear comment ready for next patter
     $("#value_comment").val("");
+    $("#value_table_container").animate({ scrollTop: $("#value_table_container").attr("scrollHeight") }, 500);
   });
 
   // clear the list of patterns
@@ -225,6 +236,12 @@ $(document).ready(function(){
     comments = new Array();
     render_list();
   });
+  
+  $("#restore_default_values").click(function() {
+    load_default_patterns();
+    render_list();
+  });
+  
  
   $("#export").click(function() {
     var text_area = $("#export_text");
@@ -232,6 +249,27 @@ $(document).ready(function(){
     text_area.focus();
     text_area.select();
   });
+ 
+   // show pattern
+  $(".action").live('click', function() {
+    switch($(this).data("action")) {
+      case "show":
+        load_pattern(patterns[$(this).data("index")]);
+        render_state();
+        break;
+      case "remove":
+        patterns.splice($(this).data("index"), 1);
+        comments.splice($(this).data("index"), 1);
+        load_pattern(0x0000);
+        render_state();
+        render_list();
+        break;
+    }
+    return false;
+  });
+ 
+ 
+ 
  
    // fill pattern
   $("#fill_pattern").click(function() {
@@ -269,5 +307,89 @@ $(document).ready(function(){
     reverse();
   });
   
+  // Defaults
+  function load_default_patterns()
+  {
+    patterns = [
+      0x446e, // A
+      0x151e, // B
+      0x0072, // C
+      0x111e, // D
+      0x4072, // E
+      0x4062, // F
+      0x047a, // G
+      0x446c, // H
+      0x1112, // I
+      0x003c, // J
+      0x4a60, // K
+      0x0070, // L
+      0x02ec, // M
+      0x08ec, // N
+      0x007e, // O
+      0x4466, // P
+      0x087e, // Q
+      0x4c66, // R
+      0x445a, // S
+      0x1102, // T
+      0x007c, // U
+      0x2260, // V
+      0x286c, // W
+      0x2a80, // X
+      0x2280, // Y
+      0x2212, // Z
+      0x227e, // 0
+      0x1100, // 1
+      0x4436, // 2
+      0x061a, // 3
+      0x444c, // 4
+      0x4852, // 5
+      0x447a, // 6
+      0x2202, // 7
+      0x447e, // 8
+      0x445e, // 9
+      0x7f80, // *
+      0x8000  // _      
+    ];
+    comments = [
+      "A", // A
+      "B", // B
+      "C", // C
+      "D", // D
+      "E", // E
+      "F", // F
+      "G", // G
+      "H", // H
+      "I", // I
+      "J", // J
+      "K", // K
+      "L", // L
+      "M", // M
+      "N", // N
+      "O", // O
+      "P", // P
+      "Q", // Q
+      "R", // R
+      "S", // S
+      "T", // T
+      "U", // U
+      "V", // V
+      "W", // W
+      "X", // X
+      "Y", // Y
+      "Z", // Z
+      "0", // 0
+      "1", // 1
+      "2", // 2
+      "3", // 3
+      "4", // 4
+      "5", // 5
+      "6", // 6
+      "7", // 7
+      "8", // 8
+      "9", // 9
+      "*", // *
+      "_"  // _      
+    ];    
+  }
   
 });
